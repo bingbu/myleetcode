54f92221a4650de2e131e606023087bbed2a0ced
Bing Bu Cao <qemukvm@gmail.com>
2017-05-31 18:17:56 +0800

    add the best solution for self crossing
    Signed-off-by: Bing Bu Cao <qemukvm@gmail.com>

--- self-acrossing.c
+++ self-acrossing.c
@@ -44,6 +44,23 @@ static bool find_dup(struct position *list) {
 
 	return false;
 }
+bool isSelfCrossing_best(int* x, int size)
+{
+	for(int i = 3; i < size; i++)
+	{
+		if(x[i]>=x[i-2]
+		   && x[i-1]<=x[i-3]) return true;
+
+		if(i>=4 && x[i-1]==x[i-3]
+		   && x[i]+x[i-4]>=x[i-2]) return true;
+
+		if(i>=5 && x[i-2]-x[i-4]>=0
+		   && x[i]>=x[i-2]-x[i-4]
+		   && x[i-1]>=x[i-3]-x[i-5]
+		   && x[i-1]<=x[i-3]) return true;
+	}
+	return false;
+}
 
 bool isSelfCrossing(int* x, int xSize) {
 	struct position *track_head;
